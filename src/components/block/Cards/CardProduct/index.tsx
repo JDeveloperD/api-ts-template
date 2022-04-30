ebaa39595bd38c080cc00c93693abffde975bd7c
David Sandoval <jsandovalbd1993@gmail.com>
2022-04-30 01:06:06 -0500

    component CardProduct - block

---
+++ src/components/block/Cards/CardProduct/index.tsx
@@ -0,0 +1,13 @@
+import CardProductStyled from './styled';
+
+const CardProduct = () => {
+  return (
+    <CardProductStyled.Wrapper>
+      <CardProductStyled.Figure>
+      </CardProductStyled.Figure>
+      <CardProductStyled.Name />
+    </CardProductStyled.Wrapper>
+  );
+};
+
+export default CardProduct;
---
+++ src/components/block/Cards/CardProduct/styled.ts
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+  width: 100%;
+  height: 200px;
+  background-color: oldlace;
+`;
+
+const Figure = styled.figure``;
+
+const Name = styled.h4``;
+
+export default {
+  Wrapper,
+  Figure,
+  Name
+};
