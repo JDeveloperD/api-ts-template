5d6c695165938c4014755bf35987a0b2294b164e
David Sandoval <jsandovalbd1993@gmail.com>
2022-04-30 15:25:37 -0500

    update component CardCourse - block

--- src/components/block/Cards/CardCourse/index.tsx
+++ src/components/block/Cards/CardCourse/index.tsx
@@ -1,9 +1,8 @@
-import { HeadLine } from '@components/typography';
-import { Badge, Button, ButtonPlay } from '@components/ui';
-import Image from 'next/image';
+import { HeadLine, Paragraph, Small } from '@components/typography';
+import { Badge, Button, ButtonPlay, Spiner } from '@components/ui';
 import CardCourseStyled from './cardcourse.styled';
 import { AiOutlineShoppingCart, AiOutlineUser } from 'react-icons/ai';
-import { FiThumbsUp } from 'react-icons/fi';
+import { FiThumbsUp, FiPlay } from 'react-icons/fi';
 
 const CardCourse = () => {
   return (
@@ -12,42 +11,60 @@ const CardCourse = () => {
        * Head Card
        */}
       <CardCourseStyled.Head>
-        <Image src="https://crehana-public-catalog.imgix.net/images/courses/promo-images/954d2f1e/f40fa18d.jpeg?auto=format&w=322&h=219&q=55&fit=crop&dpr=2" width={300} height={200} />
-        <Badge color='primary'>Básico</Badge>
-        <ButtonPlay />
+        <CardCourseStyled.Image image="https://crehana-public-catalog.imgix.net/images/courses/promo-images/954d2f1e/f40fa18d.jpeg?auto=format&w=322&h=219&q=55&fit=crop&dpr=2" />
+        <CardCourseStyled.Level>
+          <Badge color='primary'>Básico</Badge>
+        </CardCourseStyled.Level>
+        <ButtonPlay position='center'/>
+        <CardCourseStyled.Overlay>
+          <FiPlay /> Ver el trailer
+        </CardCourseStyled.Overlay>
       </CardCourseStyled.Head>
 
       {/**
        * Body Card
        */}
       <CardCourseStyled.Body>
-        <HeadLine size={6}>Python intermedio: funciones y algoritmosas</HeadLine>
-        <p>Por David Sandoval</p>
+        <HeadLine size={6} className="mb-1">Python intermedio: funciones y algoritmosas</HeadLine>
+        <Paragraph color='gray'>Por David Sandoval</Paragraph>
         <div>
-          <span>
+          <Small color='gray' className='d-inline-flex align-items-center gap-2'>
             <AiOutlineUser />
             3560 alumnos
-          </span>
-          <span>
+          </Small>
+          <Small color='gray' className='d-inline-flex align-items-center gap-2 ms-4'>
             <FiThumbsUp />
             100%
-          </span>
+          </Small>
         </div>
+        <CardCourseStyled.Amount className='mt-3'>
+          <HeadLine color='primary' size={5} className="mb-0">
+            $ 59.99
+          </HeadLine>
+
+          <Small color='gray' as="del">
+            $ 120.99
+          </Small>
+        </CardCourseStyled.Amount>
       </CardCourseStyled.Body>
 
       {/**
        * Foot Card
        */}
       <CardCourseStyled.Foot>
-
         <Button
           color='primary'
           variant='outlined'
           className='me-2'
+          isLoading={false}
+          componentLoading={<Spiner sm />}
         >
           <AiOutlineShoppingCart />
         </Button>
-        <Button color='primary'>Obtener Membresía</Button>
+        <Button
+          className='w-100'
+          color='primary'
+        >Obtener Membresía</Button>
       </CardCourseStyled.Foot>
     </CardCourseStyled.Wrapper>
   );
