b49877346c9ce478a40726f439cab39f4b38c309
David Sandoval <jsandovalbd1993@gmail.com>
2022-04-30 18:16:59 -0500

    create component Footer - block

---
+++ src/components/block/Footer/footer.styled.ts
@@ -0,0 +1,19 @@
+import { GRAY_SCALE } from '@styles';
+import { rgba } from 'polished';
+import styled from 'styled-components';
+
+const Wrapper = styled.footer`
+  background: ${GRAY_SCALE[1000]};
+  padding-top: 4rem;
+`;
+
+const Copy = styled.div`
+  padding: 1rem;
+  margin-top: 4rem;
+  border-top: 1px solid ${rgba(GRAY_SCALE[900], 0.3)};
+`;
+
+export default {
+  Wrapper,
+  Copy
+};
--- src/components/ui/Logo/index.tsx
+++ src/components/ui/Logo/index.tsx
@@ -1,11 +1,16 @@
 import Image from 'next/image';
 import Link from 'next/link';
+import { FC } from 'react';
 import LogoStyled from './logo.styled';
 
-const Logo = () => {
+interface LogoProps {
+  size: 'lg'
+}
+
+const Logo: FC<LogoProps> = ({ size }) => {
   return (
     <Link href="/" passHref>
-      <LogoStyled.Wrapper>
+      <LogoStyled.Wrapper size={size}>
         <Image
           src="/img/logo/horizontal/gradient.png"
           width={100}
