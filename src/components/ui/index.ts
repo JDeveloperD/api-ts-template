3bb7ab993d532404d808d01520ca1996febfc54d
David Sandoval <jsandovalbd1993@gmail.com>
2022-04-30 01:09:54 -0500

    register all components

---
+++ src/components/ui/index.ts
@@ -0,0 +1,8 @@
+export { default as Button } from './Button';
+export { default as Checkbox } from './Checkbox';
+export { default as Input } from './Input';
+export { default as Label } from './Label';
+export { default as Badge } from './Badge';
+export { default as Spiner } from './Spiner';
+export { default as Logo } from './Logo';
+export { default as ButtonPlay } from './ButtonPlay';
